ad948c386b96fd7e17f8c65959b2fbb923b7ea64
Atharv Patwardhan <84259526+atharvpatwardhan@users.noreply.github.com>
2023-04-22 17:45:01 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     <div className='flex my-10' style={{"marginLeft":"42%","marginTop":"5%"}}>
         Made by <span style={{"marginLeft":"1%"}}><a href='https://www.linkedin.com/in/atharv-patwardhan/' target='_blank'>Atharv Patwardhan</a>.</span>
     </div>
-    <p className='texl-lg text-center mb-10'>This page is created as a personal project and is not meant to imitate any real company or service.</p>
+    <p className='texl-lg text-center mb-10'>This page is created as a personal project and is not meant to resemble any real company or service.</p>
     </div>
   )
 }
